81c807d53f235f255ae549d5f00c2d9ae9eec6fd
Praveen Tekuri <praveent3373@gmail.com>
2026-08-17 07:51:31 +0530

    show data in charts or cards

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import CustomBarChart from '../components/CustomBarChart';
 import { useGlobalEmployee } from '../context/EmployeeContext'
 
@@ -16,6 +17,7 @@ interface EmployeeReducerResult {
 }
 
 const Statistics = () => {
+  const [showCharts, setShowCharts] = useState(false);
   const {employees} = useGlobalEmployee();
 
   const employeesData = employees?.employeesData || [];
@@ -53,44 +55,62 @@ const Statistics = () => {
   const formattedAverageSalary = new Intl.NumberFormat("en-In", {style: 'currency', currency: 'INR'}).format(averageSalary);
   
   const departmentsCount = Object.values(departments);
-  
+
   return (
     <div>
         <h1>Employee Statistics</h1>
-        <div className="grid grid-cols-2 gap-6 w-[60%] mx-auto">
+        <div className="grid grid-cols-7 gap-6 mx-auto">
             <div className='card p-4 rounded border text-center'>
-                <p className='text-2xl mb-2'>Total Employees</p>
-                <p className='text-xl'>{employeesData.length}</p>
+                <p className='mb-2'>Total Employees</p>
+                <p>{employeesData.length}</p>
             </div>
             <div className='card p-4 rounded border text-center'>
-                <p className='text-2xl mb-2'>Highest Salary</p>
-                <p className='text-xl'>{formattedHighest}</p>
+                <p className='mb-2'>Highest Salary</p>
+                <p>{formattedHighest}</p>
             </div>
             <div className='card p-4 rounded border text-center'>
-                <p className='text-2xl mb-2'>Lowest Salary</p>
-                <p className='text-xl'>{formattedLowest}</p>
+                <p className='mb-2'>Lowest Salary</p>
+                <p>{formattedLowest}</p>
             </div>
             <div className='card p-4 rounded border text-center'>
-                <p className='text-2xl mb-2'>Total Salary Paid</p>
-                <p className='text-xl'>{totalSalaryPaid}</p>
+                <p className='mb-2'>Total Salary Paid</p>
+                <p>{totalSalaryPaid}</p>
             </div>
             <div className='card p-4 rounded border text-center'>
-                <p className='text-2xl mb-2'>Average Salary</p>
-                <p className='text-xl'>{formattedAverageSalary}</p>
+                <p className='mb-2'>Average Salary</p>
+                <p>{formattedAverageSalary}</p>
             </div>
             <div className='card p-4 rounded border text-center'>
-                <p className='text-2xl mb-2'>Active Employees</p>
-                <p className='text-xl'>{activeEmployees}</p>
+                <p className='mb-2'>Active Employees</p>
+                <p>{activeEmployees}</p>
             </div>
             <div className='card p-4 rounded border text-center'>
-                <p className='text-2xl mb-2'>In-Active Employees</p>
-                <p className='text-xl'>{inActiveEmployees}</p>
+                <p className='mb-2'>In-Active Employees</p>
+                <p>{inActiveEmployees}</p>
             </div>
         </div>
-        <div className="grid grid-cols-2 gap-6 mt-10">
-            <CustomBarChart data = {departmentsCount} xKey="department" yKey="count"/>
-            <CustomBarChart data = {employeesData} xKey="name" yKey="salary"/>
+        <div className="mt-10">
+            <span className='font-semibold'>Show data in </span>
+            <button onClick={() => setShowCharts(true)} className='border cursor-pointer rounded p-2 mr-2'>Charts</button>
+            <button onClick={() => setShowCharts(false)} className='border cursor-pointer rounded p-2'>Cards</button>
         </div>
+        {showCharts ? (
+            <div className="grid grid-cols-2 gap-6 mt-10">
+                <CustomBarChart data = {departmentsCount} xKey="department" yKey="count"/>
+                <CustomBarChart data = {employeesData} xKey="name" yKey="salary"/>
+            </div>
+        ): (
+            <>
+            <h3 className='mt-10 font-semibold'>Department Count:</h3>
+                <div className="grid grid-cols-4 gap-6 mt-5">
+                    {departmentsCount.map((el) => (<p key={el.department} className='border rounded p-2 text-center'>{el.department} - {el.count}</p>))}
+                </div>
+                <h3 className='mt-10 font-semibold'>Salary: </h3>
+                <div className="grid grid-cols-4 gap-6 mt-5">
+                    {employeesData.map((el) => (<p key={el.id} className='border rounded p-2 text-center'>{el.name} - {el.salary}</p>))}
+                </div>
+            </>
+        )}      
     </div>
   )
 }
